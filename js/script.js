115e533f4c31edff1b7c57d6723264f6bf12111a
Grzegorz Sykut <sykutg@gmail.com>
2019-03-17 15:41:46 +0100

    Task-10.1 finished

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+	draggable: true,
+	freeScroll: true,
+	pageDots: false,
+  	cellAlign: 'center',
+  	contain: true
+});
+
+var restartButton = document.getElementById('restart-button');
+
+restartButton.addEventListener('click', function(){
+	flkty.select( 0 );
+});
+
+var progressBar = document.querySelector('.progress-bar')
+
+flkty.on( 'scroll', function( progress ) {
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
+});

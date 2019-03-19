064aeded8b4a53822d58a33a7f5f3eda00537a24
Grzegorz Sykut <sykutg@gmail.com>
2019-03-19 19:32:41 +0100

    Fix progress bar

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
 	draggable: true,
-	freeScroll: true,
+	// freeScroll: true,
 	pageDots: false,
   	cellAlign: 'center',
   	contain: true
@@ -12,6 +12,14 @@ var restartButton = document.getElementById('restart-button');
 restartButton.addEventListener('click', function(){
 	flkty.select( 0 );
 });
+
+var slideButtons = document.getElementsByTagName('a');
+
+for (var i = 0; i < slideButtons.length; i++) {
+	slideButtons[i].addEventListener('click', function () {
+		flkty.select(this.getAttribute('id'));
+	})
+}
 
 var progressBar = document.querySelector('.progress-bar')
 

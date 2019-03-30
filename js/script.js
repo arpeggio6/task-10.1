ddd9a2d1b710c7edb7b5ff16580b780f5b241a6f
Grzegorz Sykut <sykutg@gmail.com>
2019-03-30 17:39:02 +0100

    Task 10.2 finish

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+var template = document.getElementById('template').innerHTML;
+Mustache.parse(template);
+var cell = document.getElementsByClassName('carousel-cell');
+var container = document.getElementsByClassName('container');
+
+data.forEach(function(slide, i){
+	console.log(container[i]);
+	container[i].innerHTML = Mustache.render(template, slide);
+	cell[i].style.backgroundImage  = 'url('+ slide.image + ')';
+})
+
+
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
 	draggable: true,

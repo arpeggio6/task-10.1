d712f9d9d5077ab60e803a7c2a223f8fcae0436b
Grzegorz Sykut <sykutg@gmail.com>
2019-04-11 14:58:46 +0200

    Upgrade code for carousel

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
 var template = document.getElementById('template').innerHTML;
 Mustache.parse(template);
+
 var cell = document.getElementsByClassName('carousel-cell');
 var container = document.getElementsByClassName('container');
 
 data.forEach(function(slide, i){
-	console.log(container[i]);
-	container[i].innerHTML = Mustache.render(template, slide);
+	var mainCarousel = document.getElementsByClassName('main-carousel')[0];
+	var carouselCell = document.createElement('div'); 
+	var container = document.createElement('div');
+
+	mainCarousel.appendChild(carouselCell).appendChild(container);
+
+	carouselCell.classList.add("carousel-cell");
+	carouselCell.setAttribute("id", "carousel-cell" + [i+1]);
+	container.classList.add("container");
+	
+	container.innerHTML = Mustache.render(template, slide);
 	cell[i].style.backgroundImage  = 'url('+ slide.image + ')';
 })
 

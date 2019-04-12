7819a745c4c97940f22035f4beaa0c9438b3e559
Grzegorz Sykut <sykutg@gmail.com>
2019-04-12 15:05:13 +0200

    Move carousel to mustache template

--- js/script.js
+++ js/script.js
@@ -6,17 +6,8 @@ var container = document.getElementsByClassName('container');
 
 data.forEach(function(slide, i){
 	var mainCarousel = document.getElementsByClassName('main-carousel')[0];
-	var carouselCell = document.createElement('div'); 
-	var container = document.createElement('div');
 
-	mainCarousel.appendChild(carouselCell).appendChild(container);
-
-	carouselCell.classList.add("carousel-cell");
-	carouselCell.setAttribute("id", "carousel-cell" + [i+1]);
-	container.classList.add("container");
-	
-	container.innerHTML = Mustache.render(template, slide);
-	cell[i].style.backgroundImage  = 'url('+ slide.image + ')';
+	mainCarousel.innerHTML += Mustache.render(template, slide);
 })
 
 

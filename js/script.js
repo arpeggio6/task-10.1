023eee2735aee9611a9d23abd8b9ee7358674789
Grzegorz Sykut <sykutg@gmail.com>
2019-04-13 21:25:35 +0200

    Move carousel code into mustache template

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 var template = document.getElementById('template').innerHTML;
 Mustache.parse(template);
-var cell = document.getElementsByClassName('carousel-cell');
-var container = document.getElementsByClassName('container');
+var mainCarousel = document.querySelector('.main-carousel');
+
+data.forEach(function(slide){
+	mainCarousel.innerHTML += Mustache.render(template, slide);
+});
 
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
@@ -27,21 +30,15 @@ flkty.on( 'scroll', function( progress ) {
   progressBar.style.width = progress * 100 + '%';
 });
 
-
-
 window.initMap = function() {
   // The location of Uluru
   var uluru = {lat: 51.107713, lng: 17.032452};
   // The map, centered at Uluru
   var map = new google.maps.Map(
       document.getElementById('map'), {zoom: 10, center: uluru});
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: uluru, map: map});
+  var marker = [];
 
 	data.forEach(function(slide, i){
-		container[i].innerHTML = Mustache.render(template, slide);
-		cell[i].style.backgroundImage  = 'url('+ slide.image + ')';
-	  	var marker = [];
 		marker[i] = new google.maps.Marker({
 			position: slide.coords,
 			map: map
@@ -50,9 +47,8 @@ window.initMap = function() {
 			event.preventDefault();
 			flkty.select( i );
 		});	
-		
-		flkty.on( 'change', function( index ) {
+	});
+	flkty.on( 'change', function( index ) {
 			map.panTo(data[index].coords)
-		});
-	})
+	});
 }
